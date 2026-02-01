1fd03aeb228a45ba2749ac22474b109d3d33640d
Kyle Jeong <77771518+Kylejeong2@users.noreply.github.com>
2026-01-31 16:12:19 -0800

    adding browserbase packages to external next packages (#8)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@browserbasehq/stagehand", "@browserbasehq/sdk"],
   images: {
     domains: ['imgflip.com', 'i.imgflip.com'],
     remotePatterns: [
